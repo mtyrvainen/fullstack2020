5733cdda84762f541ea2499a3950639149aad8eb
MT <marko.tyrvainen@gmail.com>
2020-04-11 21:26:20 +0300

    osa2 puhelinluettelo 2.15-2.20

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from 'react'
 import Contacts from './components/Contacts'
 import FilterForm from './components/FilterForm'
 import PersonForm from './components/PersonForm'
+import Notification from './components/Notification'
 import contactService from './services/contactservice'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
-
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ contactFilter, setContactFilter ] = useState('')
-
+  const [ notificationText, setNotificationText ] = useState(null)
+  const [ notificationType, setNotificationType ] = useState('notification')
+ 
   const handleNameChange = (event) => setNewName(event.target.value)
   const handleNumberChange = (event) => setNewNumber(event.target.value)
   const handleFilterChange = (event) => setContactFilter(event.target.value)
@@ -31,6 +33,14 @@ const App = () => {
     }) 
   }
 
+  const displayNotification = (text, type) => {
+    setNotificationType(type)
+    setNotificationText(text)
+    setTimeout( () => {
+      setNotificationText(null)
+    }, 5000)
+  }
+
   const updateName = (oldName, updatedNumber) => {
     const person = persons.find(person => person.name === oldName)
     const updatedPerson = { ...person, number: updatedNumber }
@@ -39,6 +49,11 @@ const App = () => {
       .update(person.id, updatedPerson)
       .then(existingPerson => {
         setPersons(persons.map(person => person.id !== updatedPerson.id ? person : updatedPerson))
+        displayNotification(`Number for '${oldName}' updated`, 'notification')
+      })
+      .catch(error => {
+        displayNotification(`ERROR: ${oldName} has already been removed from server`, 'error')
+        setPersons(persons.filter(p => p.id !== updatedPerson.id))
       })
   } 
 
@@ -68,24 +83,27 @@ const App = () => {
         setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
+        displayNotification(`'${newName} ${newNumber}' added to phonebook`, 'notification')
       })
   }
 
   const removeName = id => {
-    
     if (window.confirm(`Delete '${persons.find(p => p.id === id).name}'?`)) {
+      let deleted = persons.find(p => p.id === id).name
 
-    contactService
-      .remove(id)
-      .then(response => {
-        setPersons(persons.filter(person => person.id !== id))
-      })
+      contactService
+        .remove(id)
+        .then(response => {
+          setPersons(persons.filter(person => person.id !== id))
+          displayNotification(`'${deleted}' has been removed from phonebook`, 'notification')
+        })
     }
   }
-
+  
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification text={notificationText} type={notificationType} />
       <FilterForm filterValue={contactFilter} filterFunction={handleFilterChange} />
 
       <h2>Add a new contact</h2>
---
+++ osa2/puhelinluettelo/src/components/Notification.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Notification = ({ text, type }) => {
+    if (text === null) {
+        return null
+    }
+
+    return (
+        <div className={type}>
+            {text}
+        </div>
+    )
+}
+
+export default Notification
